f3d7ce1b15819af891eb85ffe3d4ae08117408f4
Harsh <harshvardhantomar2202@gmail.com>
2026-09-26 01:31:34 +0530

    change in condition

--- Array/2D Arrays/countIntersectionPairs.cpp
+++ Array/2D Arrays/countIntersectionPairs.cpp
@@ -6,7 +6,7 @@ int main(){
     int intersect=0;
     vector<vector<int>> arr={{1,2},{3,4},{5,6}};
     for(int i=0;i<arr.size();i++){
-        for(int j=i+1;j<=arr.size()-1;j++){
+        for(int j=i+1;j<arr.size();j++){
             if(max(arr[i][0],arr[j][0])<=min(arr[i][1],arr[j][1])){
                 intersect++;
             }
