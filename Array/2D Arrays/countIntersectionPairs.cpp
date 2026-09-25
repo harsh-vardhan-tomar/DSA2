c7c3c4f5c384553cf95241f0bb73da24890adff3
Harsh <harshvardhantomar2202@gmail.com>
2026-09-25 23:09:30 +0530

    Count Intersection pairs in array using brute force approach

---
+++ Array/2D Arrays/countIntersectionPairs.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    int intersect=0;
+    vector<vector<int>> arr={{1,2},{3,4},{5,6}};
+    for(int i=0;i<arr.size();i++){
+        for(int j=i+1;j<=arr.size()-1;j++){
+            if(max(arr[i][0],arr[j][0])<=min(arr[i][1],arr[j][1])){
+                intersect++;
+            }
+        }
+    }
+    cout<<intersect<<endl;
+
+}
